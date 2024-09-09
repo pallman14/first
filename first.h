7c1255d9bbc2783b30f16edfcff9418f73b7f143
Price Allman <93605381+pallman14@users.noreply.github.com>
2024-09-09 09:59:29 -0500

    Update first.h

--- first.h
+++ first.h
@@ -1,3 +1,17 @@
 // Chris, Gorana, Lian, and Price  9/6/2024
 // program: first
 // File: first.h
+
+
+#ifndef first_H
+#define first_H
+
+class productions {
+    private:
+		
+
+    public:
+
+};
+
+#endif
