b295fa3577081971222d3c2f417dc5df96a51cde
Price Allman <93605381+pallman14@users.noreply.github.com>
2024-09-09 10:44:03 -0500

    Update first.h

--- first.h
+++ first.h
@@ -6,19 +6,27 @@
 #ifndef first_H
 #define first_H
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 class productions {
     private:
-		
+	// Dictionary to store productions
+	std::unordered_map<char, std::vector<std::string>> productionRules;  
 
     public:
+	// Reads grammar from a file
 	void readGrammar();
 	// Pre-conditions:
 	// Post-conditions:
 
+	// Stores productions in a data structure (dictionary?)
 	void storeProductions();
 	// Pre-conditions:
 	// Post-conditions:
 
+	// Prints the stored productions
 	void printProductions();
 	// Pre-conditions:
 	// Post-conditions:
