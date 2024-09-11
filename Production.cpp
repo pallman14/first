4af7b291913ff171f2f84c80f5a037834c62afc6
Price Allman <93605381+pallman14@users.noreply.github.com>
2024-09-11 00:38:00 -0500

    comments for Production.cpp

--- Production.cpp
+++ Production.cpp
@@ -1,19 +1,27 @@
 #include "Production.h"
 
+// Constructor for the Production class
+// This initializes a production rule with a LHS non-terminal and a RHS string
 Production::Production(NonTerminal* lhsSymbol, const char* rhsString) {
-    lhs = lhsSymbol;
+    // Assign LHS NonTerminal object
+    lhs = lhsSymbol
+    // Copy the RHS string into the 'rhs' array
     strcpy(rhs, rhsString);
 }
 
+// Returns a pointer to the NonTerminal object representing the LHS of the production rule
 const NonTerminal* Production::getLHS() const {
+    // Return the NonTerminal object for the LHS
     return lhs;
 }
 
+// Returns the RHS of the production rule as a C-style string
 const char* Production::getRHS() const {
+    // Return the RHS string
     return rhs;
 }
 
+// Destructor for the Production class
 Production::~Production() {
-    // Destructor for Production does not need to free the NonTerminal, 
-    // assuming it's managed by another class (Grammar).
+  
 }
