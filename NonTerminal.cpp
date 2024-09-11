1b82dd8bc2891ee53d819686e015d1203ebf654a
Price Allman <93605381+pallman14@users.noreply.github.com>
2024-09-11 00:28:29 -0500

    comments for NonTerminal.cpp

--- NonTerminal.cpp
+++ NonTerminal.cpp
@@ -1,13 +1,20 @@
 #include "NonTerminal.h"
 
+// Initializes a NonTerminal object with the given symbol.
+// Parameters: const char* sym. A string representing the symbol for the non-terminal.
 NonTerminal::NonTerminal(const char* sym) {
+    // Copy the symbol into the member variable using strcpy
+    // Assumes that 'sym' is a valid, null-terminated string
     strcpy(symbol, sym);
 }
 
+// Getter function for the non-terminal symbol.
 const char* NonTerminal::getSymbol() const {
+    // Return the symbol stored in the NonTerminal object.
     return symbol;
 }
 
+// No dynamic memory to clean up in this case as of now.
 NonTerminal::~NonTerminal() {
-    // No dynamic memory to clean up in this case.
+    
 }
